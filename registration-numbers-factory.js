7c9b86fc77019ee965560503c760e3b47accb1ce
michael <michaelcdavids@gmail.com>
2018-08-29 18:13:42 +0200

    added validation to entered number plates

--- registration-numbers-factory.js
+++ registration-numbers-factory.js
@@ -2,16 +2,30 @@ module.exports = function (pool) {
 
     async function addRegistrationNumber(number) {
         let startString = number.substring(0, 3).trim();
-        let found = await pool.query('select id from towns where starts_with=$1 ', [startString]);
-        if (found.rowCount > 0) {
-            let foundRegistration = await pool.query('select id from registrations where reg_number=$1', [number]);
-            if (foundRegistration.rowCount > 0) {
-                return "Number already exists";
+        number.toUpperCase();
+        allowedCharacters = /^[\w ]+$/;
+        if (number.match(allowedCharacters)) {
+            let found = await pool.query('select id from towns where starts_with=$1 ', [startString]);
+            if (number.length >= 3) {
+                if (number.charAt(0) !== '' && startString !== '') {
+                    if (found.rowCount > 0) {
+                        let foundRegistration = await pool.query('select id from registrations where reg_number=$1', [number]);
+                        if (foundRegistration.rowCount > 0) {
+                            return "vehicle registration number already exists in database";
+                        }
+                        await pool.query('insert into registrations (reg_number,town_id) values($1,$2)', [number, found.rows[0].id]);
+                        return 'vehicle registration has added successfully!';
+                    }
+                    return `${startString} is not a valid town identifier`
+                } else {
+                    return `${number} not a valid western cape vehicle registration number`
+                }
+            } else {
+                return "vehicle registration number is too short";
             }
-            await pool.query('insert into registrations (reg_number,town_id) values($1,$2)', [number, found.rows[0].id]);
-            return 'Registration added successfully';
+        }else{
+            return "vehicle registration number has characters that are not allowed"
         }
-        return `${startString} is not a valid town tag`
     }
 
     async function townList() {
@@ -25,7 +39,7 @@ module.exports = function (pool) {
     }
 
     async function regFilter(town) {
-        if (town === "" || town === undefined) {
+        if (town === 'all' || town === "" || town === undefined) {
             let list = await pool.query('select reg_number from registrations');
             return list.rows;
         } else {
