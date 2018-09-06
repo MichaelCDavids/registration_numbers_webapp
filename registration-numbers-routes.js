2ef431ea4166f994ce09a3f4f9a750d6f1aef674
michael <michaelcdavids@gmail.com>
2018-09-06 10:45:29 +0200

    refactored code

--- registration-numbers-routes.js
+++ registration-numbers-routes.js
@@ -1,58 +1,56 @@
 module.exports = function (registrationNumbersInstance) {
-  async function index (req, res) {
-    let data = {
-      towns: await registrationNumbersInstance.getTowns(),
-      registrations: await registrationNumbersInstance.filterRegistration()
+    async function index (req, res) {
+        let data = {
+            towns: await registrationNumbersInstance.getTowns(),
+            registrations: await registrationNumbersInstance.filterRegistration()
+        };
+
+        res.render('index', {
+            data
+        });
     }
 
-    res.render('index', {
-      data
-    })
-  }
+    async function registration_numbers_get (req, res) {
+        let town = req.params.location;
+        if (town === 'all' || town === '') {
+            var data = {
+                towns: await registrationNumbersInstance.getTowns(),
+                registrations: await registrationNumbersInstance.filterRegistration()
+            };
+        } else {
+            var data = {
+                towns: await registrationNumbersInstance.getTowns(),
+                registrations: await registrationNumbersInstance.filterRegistration(town)
+            };
+        }
 
-  async function registration_numbers_get (req, res) {
-    let town = req.params.location
-    console.log('Searching for: '+town)
-
-    if (town === 'all' || town === '') {
-      var data = {
-        towns: await registrationNumbersInstance.getTowns(),
-        registrations: await registrationNumbersInstance.filterRegistration()
-      }
-    }else {
-      var data = {
-        towns: await registrationNumbersInstance.getTowns(),
-        registrations: await registrationNumbersInstance.filterRegistration(town)
-      }
+        res.render('index', {
+            data
+        });
     }
 
-    res.render('index', {
-      data
-    })
-  }
-
-  async function registration_numbers_post (req, res) {
-    let numberPlate = req.params.plate || req.body.registrationNumber
-    formatedPlate = numberPlate.replace('%20', ' ')
-    let message = await registrationNumbersInstance.addRegistration(numberPlate)
-    let data = {
-      towns: await registrationNumbersInstance.getTowns(),
-      registrations: await registrationNumbersInstance.filterRegistration()
+    async function registration_numbers_post (req, res) {
+        let numberPlate = req.params.plate || req.body.registrationNumber;
+        formatedPlate = numberPlate.replace('%20', ' ');
+        let message = await registrationNumbersInstance.addRegistration(numberPlate);
+        let data = {
+            towns: await registrationNumbersInstance.getTowns(),
+            registrations: await registrationNumbersInstance.filterRegistration()
+        };
+        req.flash('info', message);
+        res.render('index', {
+            data
+        });
     }
-    req.flash('info', message)
-    res.render('index', {
-      data
-    })
-  }
 
-  function redirect (req, res) {
-    res.redirect('/')
-  }
+    function redirect (req, res) {
+        res.redirect('/');
+    }
 
-  return {
-    index,
-    registration_numbers_get,
-    registration_numbers_post,
-    redirect
-  }
-}
+    return {
+        index,
+        registration_numbers_get,
+        registration_numbers_post,
+        redirect
+    };
+};
