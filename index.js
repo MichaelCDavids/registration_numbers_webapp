8e14fe2835bb1ca84886dc8e095a0317417fde78
michael <michaelcdavids@gmail.com>
2018-09-06 10:45:08 +0200

    refactored code

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-const flash = require('express-flash')
-const session = require('express-session')
-const express = require('express')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const RegistrationNumbers = require('./registration-numbers-factory')
-const RegistrationRoutes = require('./registration-numbers-routes')
+const flash = require('express-flash');
+const session = require('express-session');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const RegistrationNumbers = require('./registration-numbers-factory');
+const RegistrationRoutes = require('./registration-numbers-routes');
 
-const pg = require('pg')
-const Pool = pg.Pool
+const pg = require('pg');
+const Pool = pg.Pool;
 
-let useSSL = false
-let local = process.env.LOCAL || false
+let useSSL = false;
+let local = process.env.LOCAL || false;
 
 if (process.env.DATABASE_URL && !local) {
-    useSSL = true
+    useSSL = true;
 }
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registration_numbers'
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registration_numbers';
 
 const pool = new Pool({
     connectionString,
     ssl: useSSL
-})
+});
 
-let app = express()
+let app = express();
 
 app.use(session({
     secret: 'This is a secret message',
     resave: false,
     saveUninitialized: true
-}))
+}));
 app.use(flash());
 
-let RegistrationNumbersInstance = RegistrationNumbers(pool)
-let registrationRoutes = RegistrationRoutes(RegistrationNumbersInstance)
+let RegistrationNumbersInstance = RegistrationNumbers(pool);
+let registrationRoutes = RegistrationRoutes(RegistrationNumbersInstance);
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
-}))
+}));
 
-app.set('view engine', 'handlebars')
-app.use(express.static('public'))
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-app.use(bodyParser.json())
+}));
+app.use(bodyParser.json());
 
-app.get("/", registrationRoutes.index);
-app.get("/reg_number", registrationRoutes.registration_numbers_get);
-app.post("/reg_number", registrationRoutes.registration_numbers_post);
-app.get("/reg_numbers", registrationRoutes.registration_numbers_get);
-app.get("/reg_number/:plate", registrationRoutes.registration_numbers_post);
-app.get("/reg_numbers/:location", registrationRoutes.registration_numbers_get);
+app.get('/', registrationRoutes.index);
+app.get('/reg_number', registrationRoutes.registration_numbers_get);
+app.post('/reg_number', registrationRoutes.registration_numbers_post);
+app.get('/reg_numbers', registrationRoutes.registration_numbers_get);
+app.get('/reg_number/:plate', registrationRoutes.registration_numbers_post);
+app.get('/reg_numbers/:location', registrationRoutes.registration_numbers_get);
 
 let PORT = process.env.PORT || 3008;
 
